9705fc1db9bcf1e158ab6e34125238c6defbf947
row-zeal <rojilnem.shrestha@gmail.com>
2023-06-01 20:47:47 +0545

    Merge remote changes

---
+++ codex project/api/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .api_students import *
+from .api_canidates import *
+from .api_counting_voters import *
+from .api_choosing_winner import *
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def show_students_details(request):
+    return Response(api_student_details(request=request))
+
+@api_view(['GET'])
+def show_candidates_details(request):
+    return Response(api_candidates_details(request=request))
+
+@api_view(['GET'])
+def show_winner_details(request):
+    return Response(api_winner_details(request=request))
+
+
+@api_view(['POST'])
+def add_vote_count(request):
+    return Response(api_vote_count(request.data))
